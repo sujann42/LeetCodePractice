717e9c3c2eeebeaccafaf21f6a6226c01227f855
Sujan L <sujann42@gmail.com>
2020-01-18 13:32:51 -0600

    Update PalindromeTest.java

--- src/com/palindrome/PalindromeTest.java
+++ src/com/palindrome/PalindromeTest.java
@@ -11,6 +11,7 @@ public class PalindromeTest {
 		System.out.println(palindrome(word));
 	}
 
+	//Using String Buffer
 	public static String testPalindrome(String word) {
 		StringBuffer sb = new StringBuffer(word);
 		StringBuffer reverseWord = new StringBuffer(word);
@@ -28,6 +29,7 @@ public class PalindromeTest {
 		return result;
 	}
 
+	//Using string and a for loop to reverse a string
 	public static String palindrome(String word) {
 		String reversedWord = "";
 		for (int i = word.length() - 1; i >= 0; i--) {
@@ -37,6 +39,7 @@ public class PalindromeTest {
 		System.out.println("    Word is: " + word);
 		System.out.println("Reversed is: " + reversedWord);
 
+		//Comparing two string to make sure they have the same content.
 		if ((word.equals(reversedWord))) {
 			result = "Its a palindrome";
 
