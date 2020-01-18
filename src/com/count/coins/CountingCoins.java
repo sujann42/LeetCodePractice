dfb7d4d952a471b4ea593097bb55eeb498811561
suzan <sujann42@gmail.com>
2020-01-18 13:28:16 -0600

    palindrome

--- src/com/count/coins/CountingCoins.java
+++ src/com/count/coins/CountingCoins.java
@@ -10,20 +10,20 @@ public class CountingCoins {
 	private static int reminder = 0;
 
 	public static void main(String[] args) {
-		//Converting given array to a list
-		ArrayList<Integer> coinsDenomination = new ArrayList<>(Arrays.asList(50,100,20,10,1,5));
-		int totalAmount = 573;
+		// Converting given array to a list
+		ArrayList<Integer> coinsDenomination = new ArrayList<>(Arrays.asList(50, 100, 20, 10, 1, 5));
+		int totalAmount = 179;
 		countCoins(coinsDenomination, totalAmount);
 
 	}
 
 	public static void countCoins(List<Integer> coinsDenomination, int totalAmount) {
-		//Sorting elements in the list in descending order
+		// Sorting elements in the list in descending order
 		Collections.sort(coinsDenomination, Collections.reverseOrder());
-		
-		//Looping over list
+
+		// Looping over list
 		for (int i : coinsDenomination) {
-			//Looping over list if the value of the total amount >= denomination.
+			// Looping over list if the value of the total amount >= denomination.
 			if (totalAmount >= i) {
 				count += totalAmount / i;
 				totalAmount %= i;
